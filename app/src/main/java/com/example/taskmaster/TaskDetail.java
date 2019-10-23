223dceba19c4320ab13d7b4f345eadc1af6dede8
Sharina Stubbs <sharinastubbs@gmail.com>
2019-10-23 13:15:53 -0700

    Added three hard coded task buttons to main page. First button, Go Food Shopping, when clicks, sends user to details page, where that button's text is the title of the page

--- app/src/main/java/com/example/taskmaster/TaskDetail.java
+++ app/src/main/java/com/example/taskmaster/TaskDetail.java
@@ -3,6 +3,7 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TaskDetail extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class TaskDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_detail);
+
+
+        // get the data added to the intent & use it to display this activity
+        String task = getIntent().getStringExtra("task");
+
+        // grab the label from the page
+        TextView view = findViewById(R.id.detailTaskTitle);
+        // set its text to be the item name
+        view.setText(task);
     }
 }
