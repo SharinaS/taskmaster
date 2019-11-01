3807e01731473fbc2029bd4e268d684f83185f38
Sharina Stubbs <sharinastubbs@gmail.com>
2019-11-01 13:29:39 -0700

    fixed duplication issue in database by moving awsAppSyncClient from onCreate to onResume in MainActivity, built spinner and code to allow user to choose a team, which then goes into dynamoDB

--- app/src/main/java/com/example/taskmaster/TaskAdapter.java
+++ app/src/main/java/com/example/taskmaster/TaskAdapter.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.Arrays;
 import java.util.List;
 
 class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
