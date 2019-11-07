18a9ba0da57a31c03e9d8f87d7d4bc934b338bc3
Sharina Stubbs <sharinastubbs@gmail.com>
2019-11-07 13:44:24 -0800

    code exists in GetDetail - that is not yet done - that will ultimately show image obtained from DB

--- app/src/main/java/com/example/taskmaster/TaskDetail.java
+++ app/src/main/java/com/example/taskmaster/TaskDetail.java
@@ -3,7 +3,17 @@ package com.example.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+import java.io.File;
 
 public class TaskDetail extends AppCompatActivity {
 
@@ -28,11 +38,49 @@ public class TaskDetail extends AppCompatActivity {
 
         // == Get the String of the image to ultimately render it
         // get the string from the intent
-        String taskImage = getIntent().getStringExtra("taskImage");
+        String taskImageStr = getIntent().getStringExtra("taskImage");
         TextView imageView = findViewById(R.id.testImageString);
-        imageView.setText(taskImage);
+        imageView.setText(taskImageStr);
 
-        // plug the string into transferUtility to download image from S3
+//        // plug the string into transferUtility to download image from S3
+//        TransferUtility transferUtility =
+//                TransferUtility.builder()
+//                        .context(getApplicationContext())
+//                        .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
+//                        .s3Client(new AmazonS3Client(AWSMobileClient.getInstance()))
+//                        .build();
+//
+//        TransferObserver downloadObserver =
+//                transferUtility.download(
+//                        taskImageStr,
+//                        new File(getApplicationContext().getFilesDir(), "downloadFromAws.jpg"));
+//
+//        downloadObserver.setTransferListener(new TransferListener() {
+//
+//            @Override
+//            public void onStateChanged(int id, TransferState state) {
+//                // grab data from downloaded file, download.txt to set image view
+//                // set the taskImageShow
+//                // getExternalFilesDir(Enironment.Directory_PICTURES)
+//                // Set image as a bitmap, then decode the file with .decodeFile(takes in the absolute path to the file
+//
+//
+//            }
+//
+//            @Override
+//            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+//                float percentDonef = ((float) bytesCurrent / (float) bytesTotal) * 100;
+//                int percentDone = (int) percentDonef;
+//
+//                Log.i("Your Activity", "   ID:" + id + "   bytesCurrent: " + bytesCurrent
+//                        + "   bytesTotal: " + bytesTotal + " " + percentDone + "%");
+//            }
+//
+//            @Override
+//            public void onError(int id, Exception ex) {
+//
+//            }
+//        });
 
     }
 }
